de77553b74dd9d5e8d20adec145690ffa830e761
VictorNunesVN <58942853+VictorNunesVN@users.noreply.github.com>
2023-05-12 19:23:54 -0300

    Update script.js
    modificações para iniciar uma programação assincrona.

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
-console.log('teste')
+console.log('Oi para uma pessoa')
+
+function mandaMensagen(){
+    console.log('Respondendo a pessoa')
+    console.log('Vou te mandar uma solocitação')
+    console.log('Solicitação recebida')
+}
+
+setTimeout(mandaMensagen,5000)
+console.log('fim da mensagem')
