6da09675964d5ce50ab8d17abe6876f94ef7d5cd
VictorNunesVN <58942853+VictorNunesVN@users.noreply.github.com>
2023-05-13 12:32:00 -0300

    alteração
    modificando o arquivo para a versão final já com o autocomplete funcionando e com o tratamento de erros.

--- js/script.js
+++ js/script.js
@@ -1,10 +1,36 @@
 // Obs: o CEP deve ter 9 digitos para cair neste erro. 
 
-async function buscaEndereco() {
-    const consultaCEP = await fetch('https://viacep.com.br/ws/01001000/json/')
-    const consultaCEPConvertida = await consultaCEP.json()
-    console.log(consultaCEPConvertida)
-}
+async function buscaEndereco(cep) {
+    const mensagemErro = document.getElementById('erro')
+    mensagemErro.innerHTML = ''
+    try {
+        const consultaCEP = await fetch(`https://viacep.com.br/ws/${cep}/json/`)
+        const consultaCEPConvertida = await consultaCEP.json()
+        if (consultaCEPConvertida.erro) {
+            throw Error('CEP inesistente')
+        }
+        const cidade = document.getElementById('cidade')
+        const logradouro = document.getElementById('endereco')
+        const estado = document.getElementById('estado')
+        const bairro = document.getElementById('bairro')
 
-buscaEndereco();
+        cidade.value = consultaCEPConvertida.localidade
+        logradouro.value = consultaCEPConvertida.logradouro
+        estado.value = consultaCEPConvertida.uf
+        bairro.value = consultaCEPConvertida.bairro
+
+        console.log(consultaCEPConvertida)
+        return consultaCEPConvertida
+    }
+    catch (erro) {
+        mensagemErro.innerHTML = `<p>CEP inválido</p>`
+        console.log(erro)
+    }
+}
+let ceps =['01001000','01001001']
+let conjuntoCeps = ceps.map(valores => buscaEndereco(valores))
+Promise.all(conjuntoCeps).then(respostas => console.log(respostas))
+
+const cep = document.getElementById('cep')
+cep.addEventListener('focusout', () => buscaEndereco(cep.value))
 
