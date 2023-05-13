b787a32f15b407b3e09a713812d1b850774ab598
VictorNunesVN <58942853+VictorNunesVN@users.noreply.github.com>
2023-05-13 11:08:42 -0300

    Update script.js
    Modificando o código com async.

--- js/script.js
+++ js/script.js
@@ -1,15 +1,10 @@
-const consultaCEP = fetch('https://viacep.com.br/ws/01001000/json/')
-    .then(resposta => resposta.json())
-    .then(r => {
-        if (r.erro) {
-            throw Error('CEP inválido!')
-        }
-        else {
-            console.log(r)
-        }
-    })
-    .catch(erro => console.log(erro))
+// Obs: o CEP deve ter 9 digitos para cair neste erro. 
 
+async function buscaEndereco() {
+    const consultaCEP = await fetch('https://viacep.com.br/ws/01001000/json/')
+    const consultaCEPConvertida = await consultaCEP.json()
+    console.log(consultaCEPConvertida)
+}
 
-console.log(consultaCEP)
+buscaEndereco();
 
